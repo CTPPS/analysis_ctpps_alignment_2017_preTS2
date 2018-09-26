dea246d0ff194c23fd1108356bc7b2d406bcbb9b
Jan Kašpar <jan.kaspar@cern.ch>
2018-09-26 17:47:44 +0200

    Update after configuration parameter rename.

--- data/phys/fill_6161/xangle_120/config_common.py
+++ data/phys/fill_6161/xangle_120/config_common.py
@@ -10,7 +10,7 @@ config.sector_45.cut_h_c = -38.4
 
 config.sector_56.cut_h_c = -39.3
 
-config.matching_1d.reference_datasets = cms.vstring("data/alig/fill_5685/xangle_120/DS1")
+config.matching.reference_datasets = cms.vstring("data/alig/fill_5685/xangle_120/DS1")
 
 config.matching_1d.rp_L_2_F.x_min = 46.5
 config.matching_1d.rp_L_2_F.x_max = 53
