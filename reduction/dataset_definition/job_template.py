5722e3a553134a01e9770fd40e4fc24401fd22ca
Jan Kašpar <jan.kaspar@cern.ch>
2018-10-23 16:23:44 +0200

    Re-reco version3: uniform pixel settings in sector 45.

--- reduction/dataset_definition/job_template.py
+++ reduction/dataset_definition/job_template.py
@@ -1,6 +1,12 @@
 import FWCore.ParameterSet.Config as cms
+from Configuration.StandardSequences.Eras import eras
 
-process = cms.Process("reduction")
+process = cms.Process("PixelReRecoAndReduction", eras.ctpps_2016)
+
+# define global tag
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '94X_dataRun2_ReReco_EOY17_v2', '')
 
 # minimum of logs
 process.MessageLogger = cms.Service("MessageLogger",
@@ -13,15 +19,22 @@ process.MessageLogger = cms.Service("MessageLogger",
 
 # data source
 process.source = cms.Source("PoolSource",
-  fileNames = cms.untracked.vstring(
-$input_files
-  ),
-
+  fileNames = cms.untracked.vstring(),
+  dropDescendantsOfDroppedBranches = cms.untracked.bool(False),
   inputCommands = cms.untracked.vstring(
     'drop *',
-    'keep CTPPSLocalTrackLites_*_*_*'
+    'keep TotemRPLocalTrackedmDetSetVector_*_*_*',
+    'keep CTPPSDiamondLocalTrackedmDetSetVector_*_*_*',
+    'keep CTPPSPixelRecHitedmDetSetVector_*_*_*'
   )
 )
+
+from input_files import AddFiles
+AddFiles(process.source)
+
+#process.maxEvents = cms.untracked.PSet(
+#  input = cms.untracked.int32(100)
+#)
 
 # apply JSON file
 import FWCore.PythonUtilities.LumiList as LumiList
@@ -31,14 +44,26 @@ JSONfile = 'run_ls_selection.json'
 myLumis = LumiList.LumiList(filename = JSONfile).getCMSSWString().split(',')
 process.source.lumisToProcess.extend(myLumis)
 
-# TODO: comment out
-#process.maxEvents = cms.untracked.PSet(
-#  input = cms.untracked.int32(100)
-#)
+# geometry
+process.load("Geometry.VeryForwardGeometry.geometryRPFromDD_2017_cfi")
+
+# load reco sequences
+process.load("RecoCTPPS.Configuration.recoCTPPS_sequences_cff")
+
+#process.dump = cms.EDAnalyzer("EventContentAnalyzer")
+
+process.p = cms.Path(
+    process.ctppsPixelLocalTracks *
+    process.ctppsLocalTrackLiteProducer
+)
 
 # output configuration
 process.output = cms.OutputModule("PoolOutputModule",
-  fileName = cms.untracked.string("$output_file")
+  fileName = cms.untracked.string("$output_file"),
+  outputCommands = cms.untracked.vstring(
+    "drop *",
+    'keep CTPPSLocalTrackLites_*_*_*'
+  )
 )
 
 process.outpath = cms.EndPath(process.output)
