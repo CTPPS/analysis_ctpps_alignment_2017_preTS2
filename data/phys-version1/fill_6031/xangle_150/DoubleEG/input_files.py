fac3886c8bfc30af814ea4495192d674385747cc
Jan Kašpar <jan.kaspar@cern.ch>
2019-04-10 12:43:17 +0200

    Removed auto-generated files.

--- data/phys-version1/fill_6031/xangle_150/DoubleEG/input_files.py
+++
@@ -1,5 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-input_files = cms.vstring(
-    "root://eostotem.cern.ch//eos/totem/data/ctpps/reconstruction/2017/preTS2_alignment_data/version1/fill6031_xangle150_DoubleEG.root"
-)
