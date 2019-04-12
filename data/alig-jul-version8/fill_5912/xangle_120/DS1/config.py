98338158097f300462cb4ffe3183f396ae461fb0
Jan Kašpar <jan.kaspar@cern.ch>
2019-04-12 17:08:41 +0200

    Configs updated.

--- data/alig-jul-version8/fill_5912/xangle_120/DS1/config.py
+++ data/alig-jul-version8/fill_5912/xangle_120/DS1/config.py
@@ -44,14 +44,14 @@ config.matching_1d.rp_R_1_F.x_max = 3.6
 config.matching_1d.rp_R_2_F.x_min = 2.0
 config.matching_1d.rp_R_2_F.x_max = 3.4
 
-config.x_alignment_meth_o.rp_L_2_F.x_min = 1.5
-config.x_alignment_meth_o.rp_L_2_F.x_max = 6.
+config.x_alignment_meth_o.rp_L_2_F.x_min = 2.5
+config.x_alignment_meth_o.rp_L_2_F.x_max = 4.3
 
-config.x_alignment_meth_o.rp_L_1_F.x_min = 1.5
-config.x_alignment_meth_o.rp_L_1_F.x_max = 6.
+config.x_alignment_meth_o.rp_L_1_F.x_min = 2.8
+config.x_alignment_meth_o.rp_L_1_F.x_max = 4.5
 
-config.x_alignment_meth_o.rp_R_1_F.x_min = 1.5
-config.x_alignment_meth_o.rp_R_1_F.x_max = 6.
+config.x_alignment_meth_o.rp_R_1_F.x_min = 2.2
+config.x_alignment_meth_o.rp_R_1_F.x_max = 4.0
 
-config.x_alignment_meth_o.rp_R_2_F.x_min = 1.5
-config.x_alignment_meth_o.rp_R_2_F.x_max = 6.
+config.x_alignment_meth_o.rp_R_2_F.x_min = 2.0
+config.x_alignment_meth_o.rp_R_2_F.x_max = 3.8
--- data/alig-may-version3/fill_5685/xangle_120/DS1/config.py
+++ data/alig-may-version3/fill_5685/xangle_120/DS1/config.py
@@ -22,7 +22,7 @@ config.sector_56.cut_h_a = -0.99
 config.sector_56.cut_h_c = -39.25
 config.sector_56.cut_v_c = +0.73
 
-config.matching.reference_datasets = ["data/alig-jul-version7/fill_5912/xangle_120/DS1"]
+config.matching.reference_datasets = ["data/alig-jul-version8/fill_5912/xangle_120/DS1"]
 
 config.matching_1d.rp_L_2_F.x_min = 45
 config.matching_1d.rp_L_2_F.x_max = 63
@@ -44,14 +44,14 @@ config.matching_1d.rp_R_2_F.x_max = 63
 config.matching.rp_R_2_F.sh_min = -43
 config.matching.rp_R_2_F.sh_max = -42
 
-config.x_alignment_meth_o.rp_L_2_F.x_min = 44
+config.x_alignment_meth_o.rp_L_2_F.x_min = 45.1
 config.x_alignment_meth_o.rp_L_2_F.x_max = 50
 
-config.x_alignment_meth_o.rp_L_1_F.x_min = 6
+config.x_alignment_meth_o.rp_L_1_F.x_min = 7.5
 config.x_alignment_meth_o.rp_L_1_F.x_max = 15
 
 config.x_alignment_meth_o.rp_R_1_F.x_min = 5
 config.x_alignment_meth_o.rp_R_1_F.x_max = 13
 
-config.x_alignment_meth_o.rp_R_2_F.x_min = 44.5
+config.x_alignment_meth_o.rp_R_2_F.x_min = 44.6
 config.x_alignment_meth_o.rp_R_2_F.x_max = 52
