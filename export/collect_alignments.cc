a464aef67ae40d126ed873a4cfbe02c772c47e25
Jan Kaspar <jan.kaspar@cern.ch>
2020-04-29 14:13:55 +0200

    Vertical near-far fine-tuning.

--- export/collect_alignments.cc
+++ export/collect_alignments.cc
@@ -205,8 +205,8 @@ int main()
 			if (ad.name == "sector 56") x_corr_N = +58E-3 - 82E-3/2., x_corr_F = -58E-3 + 82E-3/2.;
 
 			double y_corr_N = 0., y_corr_F = 0.;
-			if (ad.name == "sector 45") y_corr_N += +35E-3, y_corr_F += -35E-3;
-			if (ad.name == "sector 56") y_corr_N += +110E-3, y_corr_F += 0E-3;
+			if (ad.name == "sector 45") y_corr_N += +35E-3 - 25E-3, y_corr_F += -35E-3 + 25E-3;
+			if (ad.name == "sector 56") y_corr_N += +110E-3 - 25E-3, y_corr_F += 0E-3 + 25E-3;
 
 			AlignmentResult ar_N(de_x_N + x_corr_rel/2. + x_corr_N, 150E-3, d_N.sxw_y / d_N.sw_y + y_corr_N, 150E-3);
 			AlignmentResult ar_F(de_x_F - x_corr_rel/2. + x_corr_F, 150E-3, d_F.sxw_y / d_F.sw_y + y_corr_F, 150E-3);
@@ -229,7 +229,7 @@ int main()
 	}
 
 	// save results
-	output.Write("collect_alignments_2019_06_12.out");
+	output.Write("collect_alignments_2020_04_29.out");
 
 	// clean up
 	return 0;
